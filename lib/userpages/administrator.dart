bf5caa068d6d8954c53d0f67c276e26a6b3f651f
Taseen23 <taseenalam13@gmail.com>
2023-09-25 15:53:05 +0600

    Drop_down Button

--- lib/userpages/administrator.dart
+++ lib/userpages/administrator.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../utils/routes.dart';
@@ -13,157 +14,235 @@ class _AdminState extends State<Admin> {
 
   late String selectedOption;
 
-  final List<String> options = ['আমি', 'ভাত', 'খাই', ];
+  final List<String> options = ['মন্ত্রণালয়', 'পরিচালক দপ্তর', 'অন্যান্য', ];
+  final List<String> options1 = ['মন্ত্রণালয়', 'পরিচালক দপ্তর', 'অন্যান্য', ];
+  final List<String> options2 = ['মন্ত্রণালয়', 'পরিচালক দপ্তর', 'অন্যান্য', ];
+  final List<String> options3 = ['মন্ত্রণালয়', 'পরিচালক দপ্তর', 'অন্যান্য', ];
+
+  TextEditingController textEditingController = TextEditingController();
   @override
   Widget build(BuildContext context) {
 
 
     return Scaffold(
       body:
+      SingleChildScrollView(
+          child: Container(
+          child: Center(
+              child:
 
 
-      Container(
-      child: Center(
-      child:
+              Column(
+                  children: [
+                    SizedBox(
+                      height: 100.0,
+                    ),
+
+                    Text(
+                      "ব্যবহারকারী তালিকা",
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black87,
+                      ),
+                    ),
+                    Padding(
+                      padding:
+                      const EdgeInsets.symmetric(horizontal: 32.0, vertical: 16.0),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    SizedBox(
+                      width: 300,
+                      child: TextFormField
+                      //Padding(padding: Size.fromWidth(20), Size.fromHeight(30),
+                        (
+                        // controller: _firstnameController,
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white),
+                              borderRadius: BorderRadius.circular(12)
+                          ),
+                          hintText: "ব্যবহারকারীর আইডি দিন",
+                          labelText: "ব্যবহারকারীর আইডি",
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    SizedBox(
+                      width: 300,
+                      child: TextFormField(
+                        //  controller: _lastnameController,
+                        decoration: InputDecoration(
+                            suffixIcon: DropdownButton<String>(
+
+                              // value: selectedOption,
+                              onChanged: (value) {
+                                setState(() {
+                                  selectedOption = value.toString();
+                                });
+                              },
+                              items: options.map((option) {
+                                return DropdownMenuItem<String>(
+                                  value: option,
+                                  child: Text(option),
+                                );
+                              }).toList(),
+                            ),
+
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white),
+                              borderRadius: BorderRadius.circular(12)
+                          ),
+                          hintText: " --বেছে নিন-- ",
+                          labelText: "কর্মসূচি",
+                        ),
+                      ),
+                    ),
+                   // SizedBox(width: 16),
+
+                    SizedBox(
+                      height: 20.0,
+                    ),
+
+                    SizedBox(
+                      width: 300,
+                      child: TextFormField(
+
+                        //  controller: _emailController,
+
+                        decoration: InputDecoration(
+                          suffixIcon: DropdownButton<String>(
+
+                            // value: selectedOption,
+                            onChanged: (value) {
+                              setState(() {
+                                selectedOption = value.toString();
+                              });
+                            },
+                            items: options.map((option) {
+                              return DropdownMenuItem<String>(
+                                value: option,
+                                child: Text(option),
+                              );
+                            }).toList(),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white),
+                              borderRadius: BorderRadius.circular(12)
+                          ),
+                          hintText: "--বেছে নিন--",
+                          labelText: "ব্যবহারকারীর ধরণ",
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    SizedBox(
+                      width: 300,
+                      child: TextFormField(
+                        //   controller: _passwordController,
+
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          suffixIcon: DropdownButton<String>(
+
+                            // value: selectedOption,
+                            onChanged: (value) {
+                              setState(() {
+                                selectedOption = value.toString();
+                              });
+                            },
+                            items: options.map((option) {
+                              return DropdownMenuItem<String>(
+                                value: option,
+                                child: Text(option),
+                              );
+                            }).toList(),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white),
+                              borderRadius: BorderRadius.circular(12)
+                          ),
+                          hintText: "--বেছে নিন--",
+                          labelText: "স্ট্যাটাস",
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    SizedBox(
+                      width: 300,
+                      child: TextFormField(
+                        //  controller: _confirmpasswordController,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          suffixIcon: DropdownButton<String>(
+
+                            // value: selectedOption,
+                            onChanged: (value) {
+                              setState(() {
+                                selectedOption = value.toString();
+                              });
+                            },
+                            items: options.map((option) {
+                              return DropdownMenuItem<String>(
+                                value: option,
+                                child: Text(option),
+                              );
+                            }).toList(),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white),
+                              borderRadius: BorderRadius.circular(12)
+                          ),
+                          hintText: "--বেছে নিন--",
+                          labelText: "ব্যবহারকারীর রোল/(Role)",
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Container(
+                      alignment: Alignment.center,
+
+                      child: ElevatedButton(
+                        onPressed: () {
+
+                          Navigator.pushNamed(
+                            context,
+                            MyRoutes.lognipage,
+                          );
+                        }  ,
+                        child:Text('খুঁজুন'),
 
 
-      Column(
-      children: [
-        SizedBox(
-          height: 100.0,
-        ),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xffd97348),
 
-      Text(
-        "ব্যবহারকারী তালিকা",
-        style: TextStyle(
-          fontSize: 30,
-          fontWeight: FontWeight.bold,
-          color: Colors.black87,
-        ),
-      ),
-      Padding(
-        padding:
-        const EdgeInsets.symmetric(horizontal: 32.0, vertical: 16.0),
-      ),
-      SizedBox(
-        height: 20.0,
-      ),
-      SizedBox(
-        width: 300,
-        child: TextFormField
-        //Padding(padding: Size.fromWidth(20), Size.fromHeight(30),
-          (
-          // controller: _firstnameController,
-          decoration: InputDecoration(
-            enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.white),
-                borderRadius: BorderRadius.circular(12)
-            ),
-            hintText: "Enter Username",
-            labelText: "ব্যবহারকারীর আইডি",
-          ),
-        ),
-      ),
-      SizedBox(
-        height: 20.0,
-      ),
-      SizedBox(
-        width: 300,
-        child: TextFormField(
-          //  controller: _lastnameController,
-          decoration: InputDecoration(
-            enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.white),
-                borderRadius: BorderRadius.circular(12)
-            ),
-            hintText: "Enter Username",
-            labelText: "কর্মসূচি",
-          ),
-        ),
-      ),
-        SizedBox(width: 16),
-        DropdownButton<String>(
-         // value: selectedOption,
-          onChanged: (value) {
-            setState(() {
-              selectedOption = value.toString();
-            });
-          },
-          items: options.map((option) {
-            return DropdownMenuItem<String>(
-              value: option,
-              child: Text(option),
-            );
-          }).toList(),
-        ),
-      SizedBox(
-        height: 20.0,
-      ),
-      SizedBox(
-        width: 300,
-        child: TextFormField(
-          //  controller: _emailController,
+                          //Color(0xffeb6f1c),
 
-          decoration: InputDecoration(
-            enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.white),
-                borderRadius: BorderRadius.circular(12)
-            ),
-            hintText: "User Email Address",
-            labelText: "ব্যবহারকারীর ধরণ",
-          ),
-        ),
-      ),
-      SizedBox(
-        height: 20.0,
-      ),
-      SizedBox(
-        width: 300,
-        child: TextFormField(
-          //   controller: _passwordController,
+                        ),
 
-          obscureText: true,
-          decoration: InputDecoration(
-            enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.white),
-                borderRadius: BorderRadius.circular(12)
-            ),
-            hintText: "Enter New Password",
-            labelText: "স্ট্যাটাস",
-          ),
-        ),
-      ),
-      SizedBox(
-        height: 20.0,
-      ),
-      SizedBox(
-        width: 300,
-        child: TextFormField(
-          //  controller: _confirmpasswordController,
-          obscureText: true,
-          decoration: InputDecoration(
-            enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.white),
-                borderRadius: BorderRadius.circular(12)
-            ),
-            hintText: "Enter Confirm Password",
-            labelText: "ব্যবহারকারীর রোল/(Role)",
-          ),
-        ),
-      ),
-      SizedBox(
-        height: 20.0,
-      ),
-      SizedBox(
-        height: 40,
-
-
-
-      ),
-    ]
-    )
-    )
+                      ),
+                    ),
+                  ]
+              )
+          )
       )
+      )
+
+
+
     );
   }
 }
